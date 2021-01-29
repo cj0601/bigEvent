9fa6165a0c950aa50c929f5409a725a1225b0916
caojie <190723451@qq.com>
2021-01-29 19:57:42 +0800

    完成了修改用户名和修改密码的功能

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,6 +1,6 @@
 $(function () {
-  // 从 layui 中提取 form 表单模块ayer弹出层模块
-  const { form, layer } = layui
+  // 从 layui 中提取 form 表单, layer 弹出层模块
+  const { form, layer } = layui 
 
   // 1. 点击链接进行表单切换
   $('.link a').click(function () {
@@ -21,44 +21,49 @@ $(function () {
     }
   })
 
-  // 3 实现注册功能
+  // 3. 实现注册功能
   $('.reg-form').submit(function (e) {
-    // 阻止跳转
+    // 阻止默认提交
     e.preventDefault()
-    //发送ajax
+
+    // 发送 ajax
     axios.post('/api/reguser', $(this).serialize())
-      .then(res => {
-        console.log(res);
-        if (res.status !== 0) {
-          return layer.msg('注册失败!')
-        }
-        // 跳转到登录界面
-        layer.msg('注册成功!')
-        $('.login-form a').click()
-      })
+         .then(res => {
+            console.log(res)
+            // 校验失败
+            if (res.status !== 0) {
+              return layer.msg('注册失败!')
+            }
+            // 自动跳转到登录
+            layer.msg('注册成功!')
+            $('.login-form a').click()
+         })
   })
 
-  // 4 实现登录功能
+  // 4. 实现登录功能
   $('.login-form').submit(function (e) {
-    // 阻止跳转
     e.preventDefault()
-    //发送ajax
+
+    // 发送 ajax
     axios.post('/api/login', $(this).serialize())
-      .then(res => {
-        console.log(res);
-        // 效验请求失败
-        if (res.status !== 0) {
-          return layer.msg('登录失败')
-        }
-        //登录成功后，首先把token（）个人凭证保存在本地存储中
-        localStorage.setItem('token', res.token)
-        layer.msg('登录成功')
+         .then(res => {
+           console.log(res)
+          // 校验请求失败
+          if (res.status !== 0) {
+            return layer.msg('登录失败!')
+          }
 
-        // 跳转到首页
-        location.href = './index.html'
-      })
+          // 登录成功后, 首先把 token (个人身份凭证, 令牌) 保存本地存储
+          localStorage.setItem('token', res.token)
 
-
+          // 提示登录成功
+          layer.msg('登录成功!', {
+            time: 500
+          }, function () {
+            // 跳转到首页
+            location.href = './index.html'
+          })
+         })
   })
 
 })
